fd97f47427fc73b24913c3ceb1e6195e446339ed
wellerm <marci.weller@gmail.com>
2021-02-22 00:31:09 +0100

    Testplan use cases +  Finished first acceptance test (Correct submit)

--- Liferay/src/testPages/LiferayForm.java
+++ Liferay/src/testPages/LiferayForm.java
@@ -1,29 +1,76 @@
 package testPages;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class LiferayForm {
 
 	WebDriver driver;
-	public LiferayForm (WebDriver driver )  {
+	WebDriverWait wait;
+	public String errorText = "This field is required.";
+	public String successfulSubmit = "Information sent";
+	//String actual1 = driver.findElement(By.id("1")).getText();
+	
+	public LiferayForm (WebDriver driver)  {
 		this.driver = driver;
+		wait = new WebDriverWait(driver,10);
 	}
 	
-	private String errorText = "This field is required.";
+	By languageIcon = By.tagName("button");
+	
+	By otherLanguage = By.tagName("a");
 	
 	By userName = By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/form[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/input[1]");
 	
-	By userExplanationField = By.name("_com_liferay_dynamic_data_mapping_form_web_portlet_DDMFormPortlet_ddm$$WhyDidYouJoinTheTestingArea$cU3rgPQN$0$$en_US");
+	By userExplanationField = By.tagName("textarea");
 	
 	By userBirthDate = By.cssSelector(".form-control.input-group-inset.input-group-inset-after");
 	
-	public void enterUsername(String name) throws InterruptedException {
-		Thread.sleep(3000);
+	By firstDateOfDatePicker = By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/form[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div[3]/div[1]/div[1]");
+	
+	By submit = By.xpath("//*[text()='Submit']");
+	
+	public void languageChange() throws InterruptedException {
+		
+		driver.findElement(languageIcon).click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(otherLanguage));
+		driver.findElement(otherLanguage).click();
+		Thread.sleep(1000);
+		driver.navigate().refresh();
+	}
+	
+	public void enterUsername (String name) throws InterruptedException   {
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(userName));
+		driver.findElement(userName).click();
 		driver.findElement(userName).sendKeys(name);
+		Thread.sleep(1000);
+	}
+	
+	public void enterUserExplanationField (String testExperience) throws InterruptedException   {
+		driver.findElement(userExplanationField).click();
+		driver.findElement(userExplanationField).sendKeys(testExperience);
+		Thread.sleep(1000);
+	}
+	
+	public void enterBirthDate (String date) throws InterruptedException {
+		
+		driver.findElement(userBirthDate).click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(firstDateOfDatePicker));
+		driver.findElement(firstDateOfDatePicker).click();
+		Thread.sleep(1000);
+	}
+	
+	public void submitForm () throws InterruptedException {
+		driver.findElement(submit).click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("h1")));
 	}
 	
 }
--- Liferay/src/teststCases/CorrectSubmit.java
+++ Liferay/src/teststCases/CorrectSubmit.java
@@ -1,28 +1,37 @@
 package teststCases;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import testPages.LiferayForm;
 
 public class CorrectSubmit {
-
-	
 	
 	public static void main(String [] args) throws InterruptedException {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
 
-		driver.get("https://forms.liferay.com/web/forms/shared/-/form/122548?p_p_state=pop_up&p_p_auth=DmwgGHZN&_com_liferay_dynamic_data_mapping_form_web_portlet_DDMFormPortlet_languageId=en_US");	
-	
+		driver.get("https://forms.liferay.com/web/forms/shared/-/form/122548?p_p_state=pop_up&p_p_auth=m9TZZJmA&_com_liferay_dynamic_data_mapping_form_web_portlet_DDMFormPortlet_languageId=pt_BR");	
+        driver.manage().window().maximize();
 		LiferayForm form = new LiferayForm(driver);
 	
+		form.languageChange();
+		
+		
+		form.enterUsername("Teszt Elek Elemer");
+		form.enterUserExplanationField("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+		form.enterBirthDate("01092011");
+		form.submitForm();
+		
+		System.out.println(driver.findElement(By.xpath("//h1[contains(text(),'Information sent')]")).getText());
+		assertTrue((driver.findElement(By.xpath("//h1[contains(text(),'Information sent')]")).getText().equals(form.successfulSubmit)));
 	
-	form.enterUsername("Teszt");
-	
-	//driver.quit();
+		driver.quit();
 	}
 }
